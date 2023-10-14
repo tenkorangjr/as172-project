bf5930272d9eea68b14cd225a7739f521cb81dce
Michael Tenkorang <michaeltenkorang415@gmail.com>
2023-10-13 21:22:44 -0400

    Fixed issues with displaying turtle on screen window

--- star.py
+++ star.py
@@ -4,42 +4,44 @@ Class: AS172
 """
 
 from typing import List, Tuple
-from turtle import Turtle
+from turtle import RawTurtle, _Screen
 
-class Star(Turtle):
+class Star(RawTurtle):
 
-    def __init__(self, color: Tuple[int], position: Tuple[int], radius: float) -> None:
-        super().__init__()
-        self.stage_colors = []
-        self.stage_color: Tuple[int] = color
-        self.position: Tuple[float] = position
+    def __init__(self, screen: _Screen, radius: float) -> None:
+        super().__init__(screen)
+        self.stage_colors = [(0, 0, 255)]
+        self.stage_color: Tuple[int] = self.stage_colors[0]
 
         self.radius: float = radius
+
+        self.draw()
 
     def goto_main_position(self):
         self.goto((0, -1*self.radius))
 
     def draw(self):
         self.goto_main_position()
-        self.begin_fill(self.stage_color)
+        self.fillcolor(self.stage_color)
+        self.begin_fill()
         self.circle(radius = self.radius)
         self.end_fill()
 
     def still_growing(self) -> bool:
         pass
 
+    def has_cycle_ended(self) -> bool:
+        pass
+
 
 class HighMassStar(Star):
 
-    def __init__(self, color: Tuple[int], position: Tuple[int], radius: float) -> None:
-        super().__init__(color, position, radius)
+    def __init__(self, window: _Screen) -> None:
+        super().__init__(window, 50)
 
-    def draw(self):
-        pass
 
 
 class LowMassStar(Star):
 
-    def __init__(self, color: Tuple[int], position: Tuple[int], radius: float) -> None:
-        super().__init__(color, position, radius)
-        self.radius_limit: int = 100
+    def __init__(self, window: _Screen) -> None:
+        super().__init__(window, 20)
